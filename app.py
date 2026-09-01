776bc5d5ddb035acc04e2b15b2b5a03bd6f758cd
ALAKH SHUKLA <dr.alk7000@gmail.com>
2026-09-02 01:41:14 +0530

    Refactor BMR calculation logic and input handling

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 import streamlit as st
-st.title("BMR Calculator")
-name=str(st.text_input("Enter the username:"))
-dob1=int(st.number_input("enter your date of birth: "))
-dob2=int(st.number_input("enter the month of dob: "))
-dob3=int(st.number_input("enter the year of dob: "))
-age=2026-dob3
- 
-gender=str(st.text_input("male or female: "))
-height=int(st.number_input("enter your height: "))
-weight=float(st.number_input("enter your body weight: "))
-if st.button("GET RESULT"):
-    mbmr=(10*weight)+(6.25*height)-(5*age)+5
-    fbmr=(10*weight)+(6.25*height)-(5*age)-161
-   
-old=st.write("your actual age is: ",age,"years", 12-dob2,"months", 30-dob1,"days" )
-if gender=="male":
-    st.write("HI",name,"YOUR BMR=",mbmr)
 
-elif gender=="female":
-    st.write("HI",name,"YOUR BMR=",fbmr)     
-  if mbmr>1800:
-        st.write("High BMR needs fitness advice")
-    if mbmr<1400:
-        st.warning("Low BMR — please consult a doctor.")
-    st.audio("result_note.mp3")
- 
-   if fbmr>1400:
-        st.write("High BMR needs fitness advice")
-    if fbmr<1400:
-        st.warning("Low BMR — please consult a doctor.")
-    st.audio("result_note.mp3")
-         
-else:
-    st.write("Invalid data entered. Check gender Please type 'male' or 'female'.")    
+st.title("BMR Calculator")
+name = str(st.text_input("Enter the username:"))
+dob1 = int(st.number_input("enter your date of birth: "))
+dob2 = int(st.number_input("enter the month of dob: "))
+dob3 = int(st.number_input("enter the year of dob: "))
+age = 2026 - dob3
+
+gender = str(st.text_input("male or female: "))
+height = int(st.number_input("enter your height: "))
+weight = float(st.number_input("enter your body weight: "))
+
+if st.button("GET RESULT"):
+    mbmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
+    fbmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+
+    st.write("your actual age is: ", age, "years", 12 - dob2, "months", 30 - dob1, "days")
+
+    if gender == "male":
+        st.write("HI", name, "YOUR BMR=", mbmr)
+        if mbmr > 1800:
+            st.write("High BMR needs fitness advice")
+        if mbmr < 1400:
+            st.warning("Low BMR — please consult a doctor.")
+        st.audio("result_note.mp3")
+
+    elif gender == "female":
+        st.write("HI", name, "YOUR BMR=", fbmr)
+        if fbmr > 1400:
+            st.write("High BMR needs fitness advice")
+        if fbmr < 1400:
+            st.warning("Low BMR — please consult a doctor.")
+        st.audio("result_note.mp3")
+
+    else:
+        st.write("Invalid data entered. Check gender Please type 'male' or 'female'.")
+
+
 
 #credit
 st.divider()
@@ -47,4 +51,3 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
- 
