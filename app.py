86dbbcffcfba5251562bc98edf7b3ecbab664319
ALAKH SHUKLA <dr.alk7000@gmail.com>
2026-09-03 00:52:46 +0530

    Change max date for date of birth input to 2026
    Updated the maximum date for date of birth input.

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ st.title("BMR Calculator")
 
 name = str(st.text_input("Enter the username:"))
 
-dob = st.date_input("Enter your date of birth", min_value=date(1950, 1, 1), max_value=date.today())
-today = date.today()
+dob = st.date_input("Enter your date of birth", min_value=date(1950, 1, 1), max_value=date(2026, 1, 1))
+#today = date.today()
 age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
 
 gender = st.radio("Select your gender", ["male", "female"])
